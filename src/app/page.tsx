86c23923c88460dcfa73c628a819dd8c7e63345e
Michael Kihm <Michael.Kihm@rtl.de>
2025-02-18 21:32:08 +0100

    feat: header to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,13 @@ import Reil from "@/components/Reil";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen">
-      <Header />
-
-      <div
-        className="p-2 flex flex-col gap-3 flex-grow overflow-y-auto"
-        id="content-container"
-      >
-        <Apartment />
-        <Reil />
-        <div id="Buchen" className="h-72 bg-blue-500"></div>
-      </div>
+    <div
+      className="p-2 flex flex-col gap-3 flex-grow overflow-y-auto"
+      id="content-container"
+    >
+      <Apartment />
+      <Reil />
+      <div id="Buchen" className="h-72 bg-blue-500"></div>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,23 @@
 import { Link } from "@heroui/react";
 import brandImage from "public/headerbrand.png";
 import Image from "next/image";
+import { paths } from "@/paths";
 
 export default function Header() {
   const menuItems = ["Appartment", "Reil", "Buchen"];
 
   return (
-    <div className="sticky top-0 inset-x-0 bg-green-700 flex justify-between p-4 items-center text-white">
+    <div className="sticky z-40 top-0 inset-x-0 bg-green-700 flex justify-between p-4 items-center text-white">
       <div className="flex gap-1 items-center">
-        <Image
-          src={brandImage}
-          alt="Brand Image"
-          width={80}
-          height={80}
-          className="rounded-full"
-        />
+        <Link href={paths.home()}>
+          <Image
+            src={brandImage}
+            alt="Brand Image"
+            width={80}
+            height={80}
+            className="rounded-full"
+          />
+        </Link>
         <h3 className="text-3xl font-bold px-2">Römer-Apartment</h3>
       </div>
 
